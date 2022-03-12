2904f506f10e309da41ad5b373ba9649b4e55e7e
hanmant4615 <hanmantchavan2403@gmail.com>
2022-03-12 19:13:32 +0530

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+      <>
+      <div className='Nav'>
+        <nav class="navMenu">
+    <a href="/">Home</a>
+    <a href="/">Blog</a>
+    <a href="/">Work</a>
+    <a href="/">About</a>
+    <div class="dot"></div>
+  </nav>
+  </div>
+      </>
+  
+  
+  )
+}
+
+export default Navbar
