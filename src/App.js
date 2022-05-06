a5d0d7f43502c6a738a3d39a5a690e81d7d10412
hanmant4615 <hanmantchavan2403@gmail.com>
2022-05-06 12:48:02 +0530

    enviroment key

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import React, { Component } from 'react'
 import News from './components/News';
 
 export default class App extends Component {
-  apikey = process.env.REACT_APP_NEWS_API
+  apikey = process.env.REACT_APP_NEWS_API;
   render() {
     return (
       <>
       <Navbar mode='dark'/>
-<<<<<<< HEAD
-      <News />
-=======
-      <News apikey={this.apikey}/>
+      <News apikey = {this.apikey}/>
    
->>>>>>> faea709adeb7c0dae30c3e646bad3c9f7dc16feb
       </>
 
     )
